4c830f5cabfa57c2a69556d941e740bec6c8a23b
VladimirSt13 <Vladimir.Stognij@gmail.com>
2022-11-13 00:09:20 +0200

    ...

--- src/components/AuthForms/AuthForms.styled.jsx
+++ src/components/AuthForms/AuthForms.styled.jsx
@@ -12,6 +12,7 @@ export const Form = styled(FormikForm)`
 
 export const Label = styled.label`
   display: block;
+  position: relative;
   & span {
     font-weight: 700;
     display: block;
@@ -30,4 +31,35 @@ export const Input = styled(Field)`
 
 export const Button = styled.button`
   padding: 5px 10px;
+`;
+
+export const Error = styled.p`
+  font-size: 14px;
+  font-style: italic;
+  color: red;
+  position: absolute;
+  right: 30px;
+  top: 50%;
+  padding: 0;
+  margin: 0;
+  line-height: 1;
+`;
+
+export const VisibilityButton = styled.button`
+  position: absolute;
+  right: 0;
+  top: 39%;
+  background-color: transparent;
+  border: none;
+  width: 30px;
+  height: 30px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  padding: 0;
+  & svg {
+    width: 16px;
+    height: 16px;
+    color: gray;
+  }
 `;
--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,4 +1,4 @@
-import { LoginForm } from 'components/LoginForm/LoginForm';
+import { LoginForm } from 'components/AuthForms/LoginForm';
 
 export default function Login() {
   return (

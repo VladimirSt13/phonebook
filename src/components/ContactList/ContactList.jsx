051638a467a31cbc3f10d15b114ca738841bdcc9
VladimirSt13 <Vladimir.Stognij@gmail.com>
2023-08-10 00:27:52 +0300

    fix

--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -7,13 +7,22 @@ import {
 } from 'redux/contacts/selectors';
 
 export const ContactList = () => {
-  // const outputContacts = useSelector(selectVisibleContacts);
+  const outputVisibleContacts = useSelector(selectVisibleContacts);
+  console.log(
+    'file: ContactList.jsx:11  ContactList  outputVisibleContacts:',
+    outputVisibleContacts
+  );
+
   const outputContacts = useSelector(selectContacts);
+  console.log(
+    'file: ContactList.jsx:12  ContactList  outputContacts:',
+    outputContacts
+  );
 
   return (
     <List>
-      {outputContacts.length > 0 ? (
-        outputContacts.map(contact => (
+      {outputVisibleContacts.length > 0 ? (
+        outputVisibleContacts.map(contact => (
           <Contact key={contact._id} contact={contact} />
         ))
       ) : (
